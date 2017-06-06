e017b27d79988afece840e4fbd52c56c972ba260
NANKI Haruo <nanki@dotswitch.net>
2017-06-06 23:06:04 +0900

    finish.

--- lib/rspec_compose_context.rb
+++ lib/rspec_compose_context.rb
@@ -1,5 +1,24 @@
 require "rspec_compose_context/version"
 
 module RspecComposeContext
-  # Your code goes here...
+  def compose_context(*context_names, &block)
+    m = context_names.inject(self) do |m, (context_name, *args)|
+      m.module_eval do
+        str = args.map(&:inspect).join(', ')
+        str = "(#{str})" unless args.empty?
+        context("#{context_name}#{str}") do
+          include_context(context_name.to_s, *args)
+        end
+      end
+    end
+    m.module_eval &block if block_given?
+  end
+end
+
+module RSpec
+  module Core
+    class ExampleGroup
+      extend RspecComposeContext
+    end
+  end
 end
--- spec/rspec_compose_context_spec.rb
+++ spec/rspec_compose_context_spec.rb
@@ -5,7 +5,27 @@ describe RspecComposeContext do
     expect(RspecComposeContext::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  shared_context 'sum' do
+    subject do
+      a + b
+    end
+  end
+
+  shared_context 'a' do
+    metadata[:description] = "a = 1"
+
+    let :a do
+      1
+    end
+  end
+
+  shared_context 'b' do |value|
+    let :b do
+      value
+    end
+  end
+
+  compose_context :sum, :a, [:b, 2] do
+    it { is_expected.to eq 3 }
   end
 end
